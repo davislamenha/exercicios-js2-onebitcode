341effc66ac99e3988ed2a446787f597b6575a32
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-03-20 15:31:16 -0300

    elementos criados com a funcao criarElemento

--- exercicio-2/exercicio-2.js
+++ exercicio-2/exercicio-2.js
@@ -56,6 +56,85 @@ function adicionarTec() {
   const qtdDeTec = document.querySelectorAll('.tec').length;
   let tecId = qtdDeTec > 0 ? qtdDeTec + 1 : 1;
 
+  const divTec = criarElemento('div', { classList: 'tec', id: `tec${tecId}` });
+
+  const h4Tec = criarElemento('h4', { innerText: 'Tecnologia' });
+
+  const labelNome = criarElemento('label', {
+    innerText: 'Nome',
+    htmlFor: `cd-tec${tecId}-nome`,
+  });
+  const inputNome = criarElemento('input', {
+    id: `cd-tec${tecId}-nome`,
+    type: 'text',
+    placeholder: 'HTML',
+    required: '',
+  });
+
+  const h5Tec = criarElemento('h5', { innerText: 'Tempo de Experiência' });
+
+  const divRadio1 = criarElemento('div', { classList: 'radio-container' });
+  const inputRadio1 = criarElemento('input', {
+    id: `cd-tec${tecId}-exp-op1`,
+    type: 'radio',
+    value: '1 a 2 Anos',
+    name: `tec${tecId}-exp`,
+    required: '',
+  });
+  const labelRadio1 = criarElemento('label', {
+    innerText: '1 a 2 Anos',
+    htmlFor: `cd-tec${tecId}-exp-op1`,
+  });
+  divRadio1.append(inputRadio1, labelRadio1);
+
+  const divRadio2 = criarElemento('div', { classList: 'radio-container' });
+  const inputRadio2 = criarElemento('input', {
+    id: `cd-tec${tecId}-exp-op2`,
+    type: 'radio',
+    value: '3 a 4 Anos',
+    name: `tec${tecId}-exp`,
+    required: '',
+  });
+  const labelRadio2 = criarElemento('label', {
+    innerText: '3 a 4 Anos',
+    htmlFor: `cd-tec${tecId}-exp-op2`,
+  });
+  divRadio2.append(inputRadio2, labelRadio2);
+
+  const divRadio3 = criarElemento('div', { classList: 'radio-container' });
+  const inputRadio3 = criarElemento('input', {
+    id: `cd-tec${tecId}-exp-op3`,
+    type: 'radio',
+    value: 'Mais de 5 Anos',
+    name: `tec${tecId}-exp`,
+    required: '',
+  });
+  const labelRadio3 = criarElemento('label', {
+    innerText: 'Mais de 5 Anos',
+    htmlFor: `cd-tec${tecId}-exp-op3`,
+  });
+  divRadio3.append(inputRadio3, labelRadio3);
+
+  const botaoRmv = criarElemento('button', {
+    classList: 'btn-rmv',
+    id: `btn-rmv-tec${tecId}`,
+    type: 'button',
+    innerText: 'Remover Tecnologia',
+  });
+
+  divTec.append(
+    h4Tec,
+    labelNome,
+    inputNome,
+    h5Tec,
+    divRadio1,
+    divRadio2,
+    divRadio3,
+    botaoRmv,
+  );
+
+  tecContainer.append(divTec);
+
   atualizarBtnRmv();
 }
 
