c5d2c557493fa7cc419a7a27bfd71b547f9c2d91
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-03-22 14:15:36 -0300

    funcao para iniciar o jogo

--- exercicio-3/exercicio-3.js
+++ exercicio-3/exercicio-3.js
@@ -1,8 +1,21 @@
 const formulario = document.querySelector('.form');
+const tabuleiro = document.querySelector('.board');
+const casasDoTabuleiro = document.querySelectorAll('.board-item');
+const combinacoesVencedor = [
+  ['1', '2', '3'],
+  ['4', '5', '6'],
+  ['7', '8', '9'],
+  ['1', '4', '7'],
+  ['2', '5', '8'],
+  ['3', '6', '9'],
+  ['1', '5', '9'],
+  ['3', '5', '7'],
+];
 
 formulario.addEventListener('submit', function (e) {
   e.preventDefault();
   iniciarPlacar();
+  iniciarJogo();
 });
 
 function iniciarPlacar() {
@@ -18,4 +31,22 @@ function iniciarPlacar() {
 
   formulario.style.display = 'none';
   placar.style.display = 'flex';
+}
+
+function iniciarJogo() {
+  tabuleiro.dataset.start = 'on';
+  if (tabuleiro.dataset.start === 'on')
+    casasDoTabuleiro.forEach((casa) => {
+      casa.addEventListener('click', function () {
+        if (tabuleiro.dataset.turn === '1' && this.innerText === '') {
+          this.innerText = 'O';
+          tabuleiro.dataset.turn = '2';
+          verificarVencedorOuEmpate();
+        } else if (tabuleiro.dataset.turn === '2' && this.innerText === '') {
+          this.innerText = 'X';
+          tabuleiro.dataset.turn = '1';
+          verificarVencedorOuEmpate();
+        } else alert('Esta casa já está marcada!');
+      });
+    });
 }
